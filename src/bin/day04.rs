285f567d931d49e8b5f3357dc7b4fc94f523c94e
Pucha10 <puszka.dawid02@gmail.com>
2023-12-04 13:59:10 +0100

    day4 fixed version

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -4,34 +4,29 @@ fn part1(data: &str) -> i32 {
     let parsed_data = data
         .lines()
         .map(|x| {
-            x[5..]
-                .split(": ")
+            x.split(": ")
+                .last()
+                .unwrap()
+                .split(" | ")
                 .map(|x| {
-                    x.split(" | ")
-                        .map(|x| {
-                            x.split(" ")
-                                .map(|x| x.parse::<i32>().unwrap())
-                                .collect::<Vec<_>>()
-                        })
+                    x.split(" ")
+                        .map(|x| x.parse::<i32>().unwrap())
                         .collect::<Vec<_>>()
                 })
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
     let mut result = 0;
-    let mut how_many_wins = 0;
     for line in parsed_data {
-        for i in 0..line[1][0].len() {
-            for j in 0..line[1][1].len() {
-                if line[1][0][i] == line[1][1][j] {
-                    how_many_wins += 1;
-                }
+        let mut how_many_wins = 0;
+        for i in 0..line[0].len() {
+            if line[1].contains(&line[0][i]) {
+                how_many_wins += 1
             }
         }
         if how_many_wins > 0 {
             result += 2_i32.pow(how_many_wins - 1);
         }
-        how_many_wins = 0;
     }
     result
 }
@@ -41,15 +36,13 @@ fn part2(data: &String) -> usize {
     let parsed_data = data
         .lines()
         .map(|x| {
-            x[5..]
-                .split(": ")
+            x.split(": ")
+                .last()
+                .unwrap()
+                .split(" | ")
                 .map(|x| {
-                    x.split(" | ")
-                        .map(|x| {
-                            x.split(" ")
-                                .map(|x| x.parse::<i32>().unwrap())
-                                .collect::<Vec<_>>()
-                        })
+                    x.split(" ")
+                        .map(|x| x.parse::<i32>().unwrap())
                         .collect::<Vec<_>>()
                 })
                 .collect::<Vec<_>>()
@@ -58,19 +51,13 @@ fn part2(data: &String) -> usize {
     let mut scratchcards: Vec<i32> = vec![1; parsed_data.len()];
     let mut how_many_wins = 0;
     for (line_index, line) in parsed_data.iter().enumerate() {
-        for i in 0..line[1][0].len() {
-            for j in 0..line[1][1].len() {
-                if line[1][0][i] == line[1][1][j] {
-                    how_many_wins += 1;
-                }
+        for i in 0..line[0].len() {
+            if line[1].contains(&line[0][i]) {
+                how_many_wins += 1
             }
         }
-        println!("{:?}", scratchcards);
-        println!("{},{}", line_index, how_many_wins);
-        println!();
         if how_many_wins > 0 {
             for i in line_index + 1..=line_index + how_many_wins {
-                println!("{}", i);
                 scratchcards[i] += 1 * scratchcards[line_index];
             }
         }
