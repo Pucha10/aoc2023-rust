5ac96ea2ad072e6d8593b6afd09cb55a4510497e
Pucha10 <puszka.dawid02@gmail.com>
2023-12-18 01:35:48 +0100

    day12 p2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -154,6 +154,6 @@ fn part2(data: &str) -> usize {
 }
 fn main() {
     let data = read_data(12, InputType::NotTest);
-    // println!("Part 1: {}", part1(&data));
+    println!("Part 1: {}", part1(&data));
     println!("Part 2: {}", part2(&data));
 }
